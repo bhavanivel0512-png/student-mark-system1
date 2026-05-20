a62652e64e0b79dc2435e6f7f5974090a9f2f55b
bhavanivel0512 <bhavanivel0512@gamil.com>
2026-05-20 16:33:20 +0530

    deploy redy

--- studentmark/src/backend/index.js
+++ studentmark/src/backend/index.js
@@ -6,7 +6,10 @@ import studentRoutes from './routes/studentRoutes.js';
 dotenv.config();
 const app=express()
 app.use(cors({
-  origin: 'https://student-mark-entry-system1.vercel.app'
+  origin: [
+    'https://student-mark-entry-system1.vercel.app',
+    'https://student-mark-entry-system1-3d3mz5n60.vercel.app'
+  ]
 }));
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
