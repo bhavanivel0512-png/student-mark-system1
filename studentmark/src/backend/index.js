625080a5d443c24d37819107c59ebe1e33124e94
bhavanivel0512 <bhavanivel0512@gamil.com>
2026-05-20 19:50:55 +0530

    deploy redy

--- studentmark/src/backend/index.js
+++ studentmark/src/backend/index.js
@@ -3,26 +3,27 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import studentRoutes from './routes/studentRoutes.js';
+
 dotenv.config();
-const app=express()
+
+const app = express()
+
 app.use(cors({
   origin: '*'
 }));
+
 app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URI)
-  
+  .then(() => console.log("mongodb connected successfully"))
+  .catch((err) => console.log("error:", err));
 
-
-.then(()=> console.log("mongodb connected successfully"))
-.catch((err)=> console.log("error:",err));
 app.use('/api/students', studentRoutes);
 
- 
-app.get("/", (req,res)=>{
-    res.send("student mark system api ready")})
+app.get("/", (req, res) => {
+  res.send("student mark system api ready")
+})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('server running!')
-});
-
-
+})
